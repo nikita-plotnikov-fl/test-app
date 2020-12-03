746e1d78571bb71f93eb49e03c5aba29a414ac09
Nikita Plotnikov <nikita.plotnikov.fl@gmail.com>
2020-12-04 00:01:17 +0300

    learn graphql

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import {
+    GraphQLServer
+} from "graphql-yoga";
+import axios from "axios";
+
+
+const server = new GraphQLServer({
+    typeDefs: `
+        type Query {
+            agent(id:ID!): User!
+            agents: [User]
+        }
+
+        type User  {
+            id: ID
+            name: String
+            age: Int
+            married: Boolean
+            average: Float
+        }
+    `,
+    resolvers: {
+        Query: {
+            agent: async (parent, args, context, info) => {
+                const response = await axios.get(`http://localhost:3004/users/${args.id}`);
+                return response.data
+            },
+            agents: async () => {
+                const response = await axios.get('http://localhost:3004/users')
+                return response.data
+            }
+        }
+    }
+})
+
+server.start(() => {
+    console.log('And running')
+})
